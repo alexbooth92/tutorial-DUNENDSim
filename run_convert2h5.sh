51dd7f466c0c970eaab2ff6a3b0e59439d2224d5
Alexander Booth <a.c.booth92@gmail.com>
2026-04-28 17:56:23 +0530

    First go at the proper lecture format (up to end of first question done) and run_convert script for them to run with.

--- run_convert2h5.sh
+++ run_convert2h5.sh
@@ -2,21 +2,21 @@
 
 
 # Specify the run number.
-export ND_PRODUCTION_RUN_NUMBER=1
+export ND_PRODUCTION_RUN_NUMBER=__CHANGE_ME__
 
 
 # Configure the input / output file handling. 
-export ND_PRODUCTION_OUT_DIR=${PWD}/outputs
-export ND_PRODUCTION_OUT_FILE="dunend_spill.${ND_PRODUCTION_RUN_NUMBER}.hdf5"
-export ND_PRODUCTION_IN_FILE="dunend_spill.${ND_PRODUCTION_RUN_NUMBER}.root"
+export ND_PRODUCTION_OUT_DIR=__CHANGE_ME__
+export ND_PRODUCTION_OUT_FILE=__CHANGE_ME__
+export ND_PRODUCTION_IN_FILE=__CHANGE_ME__
 
 
 # Necessary due to ROOT versioning :shrug:.
 export CPATH=$EDEPSIM/include/EDepSim:$CPATH
 
 
-# Execute the conversion script.
-./includes/convert_edepsim_roottoh5.py --input_file ${ND_PRODUCTION_OUT_DIR}/${ND_PRODUCTION_IN_FILE} --output_file ${ND_PRODUCTION_OUT_DIR}/${ND_PRODUCTION_OUT_FILE}
+# Execute the conversion script. Only requires an input file path and an output file path.
+./includes/convert_edepsim_roottoh5.py __CHANGE_ME__
 
 
 echo ""
